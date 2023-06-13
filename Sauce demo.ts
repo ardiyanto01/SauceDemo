fbf9282dd5e94919696e1ec1abb949300167bc9c
Steve ardiyanto <107977803+ardiyanto01@users.noreply.github.com>
2023-06-13 16:02:34 +0700

    Add files via upload
    testing in katalon

---
+++ Sauce demo.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Sauce demo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6a51c69a-1ef7-4427-9058-381dcb6206ae</testSuiteGuid>
+   <testCaseLink>
+      <guid>a2533f5d-8059-48d8-918a-a51b7c9653ac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sicepat Test/spy_object_saucedemo_variable</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
